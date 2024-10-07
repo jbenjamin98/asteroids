4cc3b334e289373241a5c49dab8dc609f36429f6
jbenjamin98 <jacobbrianbenjamin@gmail.com>
2024-10-07 11:10:30 -0400

    Added functionality for the Player to rotate

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        player.update(dt)
+
         pygame.Surface.fill(screen, "black")
         player.draw(screen)
         pygame.display.flip()
