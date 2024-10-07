1095c4ecd43174bf3d8c5a1f8f0d4f544ef5a5a1
jbenjamin98 <jacobbrianbenjamin@gmail.com>
2024-10-07 13:40:10 -0400

    Added collision functionality

--- circleshape.py
+++ circleshape.py
@@ -13,6 +13,9 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def is_collision(self, circle_shape):
+        return self.position.distance_to(circle_shape.position) <= self.radius + circle_shape.radius
+
     def draw(self, screen):
         # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -30,6 +31,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for obj in asteroids:
+            if obj.is_collision(player):
+                print("Game over!")
+                sys.exit()
 
         pygame.Surface.fill(screen, "black")
 
