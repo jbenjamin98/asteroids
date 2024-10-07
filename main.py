188183ad3cf4034cba6a8c65f00bf1b1c276e462
jbenjamin98 <jacobbrianbenjamin@gmail.com>
2024-10-07 12:42:29 -0400

    Incorporated the use of groups in main.py - improved names

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        for item in updatable:
-            item.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
         pygame.Surface.fill(screen, "black")
 
-        for item in drawable:
-            item.draw(screen)
-            
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
